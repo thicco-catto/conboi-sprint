d8b644a8a44fa7a5426ac00b43f2cb937e4e6be9
thicco-catto <cristobal.martin01@gmail.com>
2024-05-20 13:27:35 +0200

    Added jump buffering

--- Scripts/Conboi.cs
+++ Scripts/Conboi.cs
@@ -4,73 +4,98 @@ namespace ConboiSprint;
 
 public class ConboiAnim
 {
-	public static StringName RUN = new("Run");
-	public static StringName JUMP = new("Jump");
-	public static StringName FALL = new("Fall");
+    public static StringName RUN = new("Run");
+    public static StringName JUMP = new("Jump");
+    public static StringName FALL = new("Fall");
 }
 
 public partial class Conboi : CharacterBody2D
 {
-	private float _jumpVelocity = -600.0f;
-	private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-	private float _reducedGravityMult = 0.14f;
-	private double _maxTimeHeld = 0.5d;
-	private bool _isHolding = false;
-	private double _timeHeld = 0;
+    private float _jumpVelocity = -600.0f;
+    private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    private float _reducedGravityMult = 0.14f;
+    private double _maxTimeHeld = 0.5d;
+    private double _maxJumpBuffer = 0.1d;
+    private bool _isHolding = false;
+    private double _timeHeld = 0;
+    private double _currentJumpBuffer = 0;
 
-	private GameManager _game;
-	private AnimatedSprite2D _sprite;
+    private GameManager _game;
+    private AnimatedSprite2D _sprite;
 
     public override void _Ready()
     {
-		_game = GetTree().Root.GetNode<GameManager>("Game");
-		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _game = GetTree().Root.GetNode<GameManager>("Game");
+        _sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-		_sprite.Play(ConboiAnim.RUN);
+        _sprite.Play(ConboiAnim.RUN);
     }
 
-	public override void _PhysicsProcess(double delta)
-	{
-		if(_game.State != GameState.PLAYING) {
-			return;
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_game.State != GameState.PLAYING)
+        {
+            return;
+        }
 
-		Vector2 velocity = Velocity;
+        Vector2 velocity = Velocity;
 
-		float gravity = _gravity;
+        bool onFloor = IsOnFloor();
+        bool isPressed = Input.IsActionJustPressed("ui_accept");
+        bool isHeld = Input.IsActionPressed("ui_accept");
+        float gravity = _gravity;
 
-		if(_isHolding) {
-			if(Input.IsActionPressed("ui_accept")) {
-				gravity *= _reducedGravityMult;
+		// Handle held jump
+        if (_isHolding)
+        {
+            if (isHeld)
+            {
+                gravity *= _reducedGravityMult;
 
-				_timeHeld += delta;
+                _timeHeld += delta;
 
-				if(_timeHeld >= _maxTimeHeld) {
-					_isHolding = false;
-				}
-			} else {
-				_isHolding = false;
-			}
-		}
+                if (_timeHeld >= _maxTimeHeld)
+                {
+                    _isHolding = false;
+                }
+            }
+            else
+            {
+                _isHolding = false;
+            }
+        }
 
+		// Handle gravity
+        if (!onFloor)
+        {
+            velocity.Y += gravity * (float)delta;
+            _currentJumpBuffer -= delta;
+        }
 
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
-			_isHolding = true;
-			_timeHeld = 0;
-			velocity.Y = _jumpVelocity;
-		}
+		// Handle jump
+        bool canJump = onFloor && (isPressed || _currentJumpBuffer > 0);
+        if (canJump)
+        {
+            _isHolding = true;
+            _timeHeld = 0;
+            velocity.Y = _jumpVelocity;
+            _currentJumpBuffer = 0;
+        }
 
-		if (!IsOnFloor()) {
-			velocity.Y += gravity * (float)delta;
-		}
+		// Handle jump buffering
+        if (isPressed && !onFloor)
+        {
+            _currentJumpBuffer = _maxJumpBuffer;
+        }
 
-		Velocity = velocity;
-		MoveAndSlide();
+        Velocity = velocity;
+        MoveAndSlide();
 
-		HandleAnimations();
-	}
+        HandleAnimations();
+    }
 
-	private void HandleAnimations() {
+    private void HandleAnimations()
+    {
         bool onFloor = IsOnFloor();
         bool falling = Velocity.Y > 0;
 
@@ -88,8 +113,9 @@ public partial class Conboi : CharacterBody2D
             expectedAnimation = ConboiAnim.JUMP;
         }
 
-        if (_sprite.Animation != expectedAnimation) {
-			_sprite.Play(expectedAnimation);
-		}
-	}
+        if (_sprite.Animation != expectedAnimation)
+        {
+            _sprite.Play(expectedAnimation);
+        }
+    }
 }
